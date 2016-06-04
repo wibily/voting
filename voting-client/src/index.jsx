5e1bd13303f73dccaa6dca464cbc3ff5ce127b85
wibily <wibily@gmail.com>
2016-06-04 21:14:04 +1000

    introducing routes

---
+++ voting-client/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
+import Voting from './components/Voting';
+
+const routes = <Route component={App}>
+    <Route path="/" component={Voting} />
+</Route>;
+
+ReactDOM.render(
+    <Router history={hashHistory}>{routes}</Router>,
+    document.getElementById('app')
+);
