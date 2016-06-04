f1e0ce94519e715891de094bba68a077634e8ae5
wibily <wibily@gmail.com>
2016-06-04 21:31:20 +1000

    socket.io for the client

--- voting-client/src/components/App.jsx
+++ voting-client/src/components/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import {List} from 'immutable';
-
-const pair = List.of('Trainspotting', '28 Days Later');
 
 export default React.createClass({
     render: function() {
-        return React.cloneElement(this.props.children, {pair: pair});
+        return this.props.children;
     }
 });
--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {List} from 'immutable';
 import {
     renderIntoDocument,
     scryRenderedDOMComponentsWithTag,
     Simulate
 } from 'react-addons-test-utils';
-import Voting from '../../src/components/Voting';
+import {List} from 'immutable';
+import {Voting} from '../../src/components/Voting';
 import {expect} from 'chai';
 
 describe('Voting', () => {
